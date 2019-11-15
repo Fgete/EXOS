a5221957cf358503184074d0765987b5f9b9690e
Lag <f.gete@ludus-academie.com>
2019-11-15 11:36:17 +0100

    Pascal Exo 2
    Pascal Exo 2

---
+++ exo2.pas
@@ -0,0 +1,13 @@
+program exo2;
+
+uses crt;
+var nb : integer;
+
+BEGIN
+        clrscr;
+        writeln('Entrez un entier :');
+        readln(nb);
+        writeln(nb*nb);
+        readln;
+END.
+
