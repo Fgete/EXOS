1fd7944feed515813b9d0b908b9f4527c5b71c89
Lag <f.gete@ludus-academie.com>
2019-11-15 11:59:45 +0100

    Pascal Exo 3
    Pascal Exo 3

---
+++ exo3.pas
@@ -0,0 +1,13 @@
+program exo3;
+
+uses crt;
+var nom : string;
+
+BEGIN
+        clrscr;
+        writeln('Entrez votre nom :');
+        readln(nom);
+        writeln('Bonjour ', nom);
+        readln;
+END.
+
